4c71b3d0fbfbc47b5b4a6a168aab8f0dc884fa23
me <21290320+my-oh-my@users.noreply.github.com>
2022-08-27 11:39:54 +0200

    Tests (#10)
    * Initial tests
    
    * Single api hit when testing
    
    * Version upgrade
    
    Co-authored-by: Rafał Krasowski <my-oh-my@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='expert-advisor',
-    version='0.1.6',
+    version='0.1.7',
     description='Trading Expert Advisor',
     url='https://github.com/my-oh-my/expert-advisor',
     author='my-oh-my',
