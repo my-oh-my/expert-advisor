453f022381517657d22655761fed449486ca83d4
me <21290320+my-oh-my@users.noreply.github.com>
2022-09-04 22:15:37 +0200

    Slack changes (#22)
    Co-authored-by: Rafał Krasowski <my-oh-my@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='expert-advisor',
-    version='0.2.0',
+    version='0.2.1',
     description='Trading Expert Advisor',
     url='https://github.com/my-oh-my/expert-advisor',
     author='my-oh-my',
