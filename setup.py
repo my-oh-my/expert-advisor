815727496ccf7be6578a496aaafccb45bfc4c8fa
me <21290320+my-oh-my@users.noreply.github.com>
2022-09-16 12:57:50 +0200

    Minumum lot size (#28)
    Co-authored-by: Rafał Krasowski <my-oh-my@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='expert-advisor',
-    version='0.3.2',
+    version='0.3.3',
     description='Trading Expert Advisor',
     url='https://github.com/my-oh-my/expert-advisor',
     author='my-oh-my',
