48f8c072278d1a5e1000beb5711df495060d8fb0
me <21290320+my-oh-my@users.noreply.github.com>
2022-09-18 13:14:29 +0200

    Change Order mode - limit (#30)
    Co-authored-by: Rafał Krasowski <my-oh-my@users.noreply.github.com>

--- ea/trading/backoff.py
+++ ea/trading/backoff.py
@@ -3,7 +3,7 @@ import time
 from ea.misc.logger import logger
 
 
-def retry(exception, retries=3, backoff_in_seconds=1):
+def retry(exception, retries=3, backoff_in_seconds=2):
     def decorator(f):
         def wrapper(*args, **kwargs):
             attempt = 1
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='expert-advisor',
-    version='0.3.4',
+    version='0.3.5',
     description='Trading Expert Advisor',
     url='https://github.com/my-oh-my/expert-advisor',
     author='my-oh-my',
