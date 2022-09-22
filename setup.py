797f091ab25854a62a814a860c5fcd510da253d2
Rafał Krasowski <my-oh-my@users.noreply.github.com>
2022-09-22 07:53:13 +0200

    Reasign volume

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='expert-advisor',
-    version='0.3.5',
+    version='0.3.6',
     description='Trading Expert Advisor',
     url='https://github.com/my-oh-my/expert-advisor',
     author='my-oh-my',
