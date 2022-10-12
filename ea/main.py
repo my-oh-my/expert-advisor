08f30042958df5a63949b782b36da8298b9dd6a4
Rafał Krasowski <my-oh-my@users.noreply.github.com>
2022-10-12 21:49:48 +0200

    Trading changes

--- ea/main.py
+++ ea/main.py
@@ -65,12 +65,14 @@ class EARunner:
         ea = ExpertAdvisor(ea_settings)
         raw_dataframe = ea.from_api(drop_non_closed_candle=False)
 
+        data = raw_dataframe[raw_dataframe['date_time'] <= datetime.strptime('2022-10-10 11:30:00', '%Y-%m-%d %H:%M:%S')]
+
         # Waves
         waves_settings = dict(symbol=self._settings.symbol, period=self._settings.period,
                               distance=self._settings.distance)
         waves_strategy = Waves(waves_settings)
 
-        waves_df = waves_strategy.analyze(raw_dataframe)
+        waves_df = waves_strategy.analyze(data)
 
         # Consolidation
         trailing_sl = self._settings.trailing_sl
@@ -85,8 +87,7 @@ class EARunner:
         consolidation_strategy = Consolidation(consolidation_settings)
         consolidation_df = consolidation_strategy.analyze(waves_df)
 
-        recent_consolidation_end_at = consolidation_df['recent_consolidation_end'].max()
-        since_recent_consolidation_end = consolidation_df[(consolidation_df['date_time'] > recent_consolidation_end_at)]
+        last_row = consolidation_df.iloc[-1]
 
         current_trades = ea.get_open_trades(scenario_name)
         if current_trades is not None:
@@ -100,15 +101,12 @@ class EARunner:
 
             # process active order
             for order in orders_after_execution:
-                current_trade_open_time = datetime.fromtimestamp(int(since_recent_consolidation_end['open_position_at'].max() / 1000))
-                current_trade_open_candle = self.time_floor(current_trade_open_time, timedelta(minutes=self._settings.period))
-                since_current_trade_open_candle = consolidation_df[(consolidation_df['date_time'] > current_trade_open_candle)]
                 current_stop_loss = order['sl']
                 current_trade_market = ea.get_current_trade_market(order['cmd'])
 
-                calculated_stop_loss = max([since_current_trade_open_candle['high'].max() - trailing_sl, current_stop_loss])  \
+                calculated_stop_loss = max([last_row['high'] - trailing_sl, current_stop_loss])  \
                     if current_trade_market == 'bullish' \
-                    else min([since_current_trade_open_candle['low'].min() + trailing_sl, current_stop_loss])
+                    else min([last_row['low'] + trailing_sl, current_stop_loss])
                 candidate_stop_loss = round(calculated_stop_loss, order['digits'])
                 if current_stop_loss != candidate_stop_loss:
                     logger.info(f'Modifying order with SL at: {candidate_stop_loss}')
@@ -127,12 +125,12 @@ class EARunner:
                     modification_resp = ea.modifyPosition(modification_order)
                     logger.info(modification_resp)
                     self._settings.slack.send(f'{scenario_name}: {str(modification_resp)}')
-        elif len(since_recent_consolidation_end) == 1:
+        elif last_row['iterator'] == minimum_waves_count:
             # just after required waves count is matched (1 candle ago)
             logger.info('Oposite orders placing')
-            order_input = since_recent_consolidation_end.iloc[0].to_dict()
+            order_input = last_row.to_dict()
             order_input['custom_comment'] = scenario_name
-            for position_side in list('bearish', 'bullish'):
+            for position_side in list(['bearish', 'bullish']):
                 order_input['position_side'] = position_side
                 order_resp = ea.open_order_on_signal(order_input, ea.prepare_order, ea.execute_tradeTransaction)
                 logger.info(order_resp)
